d8dbc68a97b668a40968e5e2fd348d688ea8b942
KayXDD <85695157@qq.com>
2021-01-28 16:24:18 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import axios from 'axios'
 
 import {request, instance2} from './network/request'
 
-axios.defaults.baseURL = '/chnphoto_mobile_app'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+
 
 Vue.config.productionTip = false
 
@@ -18,35 +17,5 @@ new Vue({
   store,
   render: h => h(App)
 })
-axios.post('/circle/Circle/lastestNews',{page: 1, userId: 3454}).then(res => {
-  console.log(res);
-}).catch(err => {
-  console.log(err);
-})
 
-axios({
-  url: '/circle/Circle/lastestNews',
-  method: 'post',
-  data: {page: 1, userId: 3454}
-}).then(res => {
-  console.log(res);
-}).catch(err => {
-  console.log(err);
-})
 
-// request({
-//   url: '/home/multidata',
-//   methods: 'post'
-// }).then(res => {
-//   console.log(res);
-// })
-
-// instance2({
-//   url: '/circle/Circle/lastestNews',
-//   data: JSON.stringify({page: 1, userId: '3454'}),
-//   method: 'POST'
-// }).then(res => {
-//   console.log(res);
-// }).catch(err => {
-//   console.log(err);
-// })
